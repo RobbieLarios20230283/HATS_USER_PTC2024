c207d1590cdbf5c967740f7001b8134f3f12b14f
RobbieLarios2024 <robbieavellana@gmail.com>
2024-09-01 23:45:45 -0600

    problema con el boton

--- PTC2024/app/src/main/java/hats/hats_user_ptc2024/Categorias_s.kt
+++ PTC2024/app/src/main/java/hats/hats_user_ptc2024/Categorias_s.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,11 +14,14 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [categorias.newInstance] factory method to
+ * Use the [Categorias_s.newInstance] factory method to
  * create an instance of this fragment.
  */
-class categorias : Fragment() {
+class Categorias_s : Fragment() {
+
+
     // TODO: Rename and change types of parameters
+
     private var param1: String? = null
     private var param2: String? = null
 
@@ -27,14 +31,24 @@ class categorias : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_categorias, container, false)
+        return inflater.inflate(R.layout.fragment_categorias_s, container, false)
+
+        val btnfiltroPintura: Button = view.findViewById(R.id.btnPintura_C)
+
+        btnfiltroPintura.setOnClickListener {
+        }
+
+        return view
     }
 
     companion object {
@@ -44,16 +58,19 @@ class categorias : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment categorias.
+         * @return A new instance of fragment Categorias_s.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            categorias().apply {
+            Categorias_s().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
                 }
             }
+
     }
+
+
 }
