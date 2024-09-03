c807f779a6accfc7a68f310ae018680d75d4828e
RobbieLarios2024 <robbieavellana@gmail.com>
2024-09-02 18:51:26 -0600

    Perfil actualizar

--- PTC2024/app/src/main/java/hats/hats_user_ptc2024/Perfil.kt
+++ PTC2024/app/src/main/java/hats/hats_user_ptc2024/Perfil.kt
@@ -1,23 +1,23 @@
 package hats.hats_user_ptc2024
 
+import Modelo.ClaseConexion
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import android.widget.EditText
+import android.widget.Toast
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Perfil.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Perfil : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
@@ -34,19 +34,67 @@ class Perfil : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_perfil, container, false)
+        val view = inflater.inflate(R.layout.fragment_perfil, container, false)
+
+        val txtUUIDP: EditText = view.findViewById(R.id.txtUUID_P)
+        val txtNombreP: EditText = view.findViewById(R.id.txtNombre_p)
+        val txtApellidosP: EditText = view.findViewById(R.id.txtApellidos_p)
+        val txtTelefonoP: EditText = view.findViewById(R.id.txtTelefono_p)
+        val txtCorreoP: EditText = view.findViewById(R.id.txtCorreo_p)
+        val txtDuiP: EditText = view.findViewById(R.id.txtDui_p)
+        val txtDireccionP: EditText = view.findViewById(R.id.txtDireccionP)
+        val txtfechaP: EditText = view.findViewById(R.id.txtfecha_p)
+
+        val btnActualizarP: Button = view.findViewById(R.id.btnActualizar)
+
+        btnActualizarP.setOnClickListener {
+            CoroutineScope(Dispatchers.Main).launch {
+                val nombre = txtNombreP.text.toString()
+                val apellidos = txtApellidosP.text.toString()
+                val telefono = txtTelefonoP.text.toString().toIntOrNull()
+                val dui = txtDuiP.text.toString()
+                val direccion = txtDireccionP.text.toString()
+                val fecha = txtfechaP.text.toString()
+                val uuid = txtUUIDP.text.toString()
+
+                if (nombre.isEmpty() || apellidos.isEmpty() || telefono == null || dui.isEmpty() || direccion.isEmpty() || fecha.isEmpty() || uuid.isEmpty()) {
+                    Toast.makeText(context, "Por favor, Completa todos los campos", Toast.LENGTH_SHORT).show()
+                    return@launch
+                }
+
+                try {
+                    withContext(Dispatchers.IO) {
+                        val objConexion = ClaseConexion().CadenaConexion()
+                        val sql = "UPDATE Empleador SET nombreEmpleador = ?, apellidoEmpleador = ?, Telefono = ?, DuiEmpleador = ?, Direccion = ?, fechanac = ? WHERE uuidEmpleador = ?"
+                        val preparedStatement = objConexion?.prepareStatement(sql)
+
+                        preparedStatement?.apply {
+                            setString(1, nombre)
+                            setString(2, apellidos)
+                            setInt(3, telefono)
+                            setString(4, dui)
+                            setString(5, direccion)
+                            setString(6, fecha)
+                            setString(7, uuid)
+                            executeUpdate()
+                        }
+
+                        preparedStatement?.close()
+                        objConexion?.close()
+                    }
+                    Toast.makeText(context, "Datos actualizados correctamente", Toast.LENGTH_SHORT).show()
+                } catch (e: Exception) {
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(context, "Error al actualizar datos: ${e.message}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+
+        return view
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Perfil.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             Perfil().apply {
@@ -57,3 +105,4 @@ class Perfil : Fragment() {
             }
     }
 }
+
