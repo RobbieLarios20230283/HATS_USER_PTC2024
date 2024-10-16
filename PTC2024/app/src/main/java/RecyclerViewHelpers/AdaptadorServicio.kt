60504c92dee01c2050080ebb83049e5bbcf3172d
David Zepeda <20230231@ricaldone.edu.sv>
2024-10-16 02:42:28 -0600

    Cambio menor

--- PTC2024/app/src/main/java/RecyclerViewHelpers/AdaptadorServicio.kt
+++ PTC2024/app/src/main/java/RecyclerViewHelpers/AdaptadorServicio.kt
@@ -8,7 +8,7 @@ import androidx.navigation.Navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import hats.hats_user_ptc2024.R
 
-class AdaptadorServicio (var Datos: List<tbServicios>): RecyclerView.Adapter<ViewHolderServicio>(){
+class AdaptadorServicio(var Datos: List<tbServicios>) : RecyclerView.Adapter<ViewHolderServicio>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderServicio {
         val vistaServicio = LayoutInflater.from(parent.context).inflate(R.layout.activity_item_card_servicios, parent, false)
         return ViewHolderServicio(vistaServicio)
@@ -17,12 +17,11 @@ class AdaptadorServicio (var Datos: List<tbServicios>): RecyclerView.Adapter<Vie
     override fun getItemCount() = Datos.size
 
     override fun onBindViewHolder(holder: ViewHolderServicio, position: Int) {
-
         val item = Datos[position]
         holder.txtTituloCardServicio.text = item.NombreServicios
 
-        //No tocar (Funcionando(Mostrar Detalles))
-        holder.itemView.setOnClickListener {
+        // Encuentra el ImageView por su ID dentro del ViewHolder
+        holder.iconInformacion.setOnClickListener {
             val bundle = Bundle().apply {
                 putString("NombreServicios", item.NombreServicios)
                 putString("Descripcion", item.Descripcion)
@@ -30,6 +29,5 @@ class AdaptadorServicio (var Datos: List<tbServicios>): RecyclerView.Adapter<Vie
             val navController = findNavController(holder.itemView)
             navController.navigate(R.id.detalleServicio, bundle)
         }
-
     }
 }
--- PTC2024/app/src/main/java/RecyclerViewHelpers/ViewHolderServicio.kt
+++ PTC2024/app/src/main/java/RecyclerViewHelpers/ViewHolderServicio.kt
@@ -1,10 +1,12 @@
 package RecyclerViewHelpers
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import hats.hats_user_ptc2024.R
 
-class ViewHolderServicio (view : View) : RecyclerView.ViewHolder(view){
-    val txtTituloCardServicio = view.findViewById<TextView>(R.id.txtTituloServicio)
+class ViewHolderServicio(view: View) : RecyclerView.ViewHolder(view) {
+    val txtTituloCardServicio: TextView = view.findViewById(R.id.txtTituloServicio)
+    val iconInformacion: ImageView = view.findViewById(R.id.icoinfo) // Referencia al ImageView
 }
