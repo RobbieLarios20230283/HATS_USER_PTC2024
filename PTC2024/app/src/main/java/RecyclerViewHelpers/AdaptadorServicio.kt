8e1ccf980c7dac6ed9c7ed7907f1286029e6d7ac
David Zepeda <20230231@ricaldone.edu.sv>
2024-10-16 06:14:01 -0600

    Commit pequeño

--- PTC2024/app/src/main/java/RecyclerViewHelpers/AdaptadorServicio.kt
+++ PTC2024/app/src/main/java/RecyclerViewHelpers/AdaptadorServicio.kt
@@ -4,11 +4,12 @@ import Modelo.tbServicios
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.navigation.Navigation.findNavController
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import hats.hats_user_ptc2024.R
 
 class AdaptadorServicio(var Datos: List<tbServicios>) : RecyclerView.Adapter<ViewHolderServicio>() {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderServicio {
         val vistaServicio = LayoutInflater.from(parent.context).inflate(R.layout.activity_item_card_servicios, parent, false)
         return ViewHolderServicio(vistaServicio)
@@ -20,13 +21,24 @@ class AdaptadorServicio(var Datos: List<tbServicios>) : RecyclerView.Adapter<Vie
         val item = Datos[position]
         holder.txtTituloCardServicio.text = item.NombreServicios
 
-        // Encuentra el ImageView por su ID dentro del ViewHolder
+        // Configura el OnClickListener para la tarjeta completa
+        holder.itemView.setOnClickListener {
+            val bundle = Bundle().apply {
+                putString("NombreServicios", item.NombreServicios)
+                putString("Descripcion", item.Descripcion)
+            }
+            // Navegar al ChatFragment
+            val navController = Navigation.findNavController(holder.itemView)
+            navController.navigate(R.id.chat, bundle) // Asegúrate de que el ID sea el correcto para tu ChatFragment
+        }
+
+        // Configura el OnClickListener para el icono de información
         holder.iconInformacion.setOnClickListener {
             val bundle = Bundle().apply {
                 putString("NombreServicios", item.NombreServicios)
                 putString("Descripcion", item.Descripcion)
             }
-            val navController = findNavController(holder.itemView)
+            val navController = Navigation.findNavController(holder.itemView)
             navController.navigate(R.id.detalleServicio, bundle)
         }
     }
